0bc4bf6ac775746e69822110133defbfb3da9111
Ana <anaclimgo@gmail.com>
2025-04-26 22:48:25 -0300

    inde function controlling tasks

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,3 +1,9 @@
 class TaskController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy, :toggle]
+
+  def index
+    @tasks = current_user.tasks.where(completed: false)
+    @completed_tasks = current_user.tasks.where(completed: true)
+    @total_points = current_user.tasks.where(completed: true).sum(:points)
+  end
 end
