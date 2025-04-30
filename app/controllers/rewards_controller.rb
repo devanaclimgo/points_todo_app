1d00fab911c9e5f4bf0348d9fe38590736994ece
Ana <anaclimgo@gmail.com>
2025-04-29 21:24:54 -0300

    index and new reward created

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,2 +1,12 @@
 class RewardsController < ApplicationController
+  before_action :set_reward, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @rewards = current_user.rewards.order(points_required: :asc)
+    @total_points = current_user.tasks.where(completed: true).sum(:points)
+  end
+
+  def new
+    @reward = Reward.new
+  end
 end
